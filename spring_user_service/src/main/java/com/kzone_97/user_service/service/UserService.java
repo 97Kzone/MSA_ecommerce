46a286beeb1d6bd953b205f74c194d8cc83aa94c
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-09-11 18:26:34 +0900

    22.09.11 Build. 인증, 권한 with JWT - user-service : security 이용한 로그인 기능 구현 - api-gateway : 각 요청별 route 상세화, 권한 필터 추가 - JWT token 을 이용한 사용자 조회, 정보 처리

--- spring_user_service/src/main/java/com/kzone_97/user_service/service/UserService.java
+++ spring_user_service/src/main/java/com/kzone_97/user_service/service/UserService.java
@@ -2,10 +2,13 @@ package com.kzone_97.user_service.service;
 
 import com.kzone_97.user_service.dto.UserDto;
 import com.kzone_97.user_service.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String email);
 }
